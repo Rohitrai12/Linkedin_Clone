24be6242c1c7dfb878966d5a8844f13d1125a96c
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2024-12-26 00:15:24 +0500

    some more fixes

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,17 +1,11 @@
 import mongoose from "mongoose";
-import { connectDB } from "./lib/db.js";
-
-dotenv.config();
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1);
-    }
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error connecting to MongoDB: ${error.message}`);
+		process.exit(1);
+	}
 };
